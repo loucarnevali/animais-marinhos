a235569acd574220e7340ac7d4ad7c2e9ce6309f
Louise Carnevali <loucafumee@gmail.com>
2023-05-07 14:59:13 -0400

    Accordion changed into a class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,20 +1,27 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    "[data-anime='accordion'] dt"
-  );
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
+  }
 
-  const activeClass = "ativo";
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  //ADD OS EVENTOS AO ACCORDION
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+
+  //INICIAR FUNÇÃO
+  init() {
+    if (this.accordionList.length) {
+      //ATIVAR PRIMEIRO ITEM
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
